6c75f7dd534e3462c6a3348b55986eec090cee10
sajaldiu133 <sajaldiu133@gmail.com>
2026-01-02 01:53:05 +0600

    if_elif_else( vote_marriage)

---
+++ vote_m.py
@@ -0,0 +1,12 @@
+# Vaote and Marrige
+
+age = int(input('Enter Age = '))
+
+if age >= 21:
+    print(" can VOTE and MARRIGE ")
+
+elif age < 21 and age >=  18   :
+    print("can VOTE but not MARRIGE")
+    
+else:
+    print("NOTHING")
