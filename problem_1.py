da7263e44444874d9c00e00541f9aa3041f0dd3b
sajaldiu133 <sajaldiu133@gmail.com>
2026-01-01 23:52:09 +0600

    beecrowd_2748

---
+++ problem_1.py
@@ -0,0 +1,10 @@
+#beecrowd_2748
+
+
+print("-" * 39)
+print("|        Roberto                      |")
+print("|                                     |")
+print("|        5786                         |")
+print("|                                     |")
+print("|        UNIFEI                       |")
+print("-" * 39)
